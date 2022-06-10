14422295eeb319f0f3fd32e4b0d47e41d7fe522e
diegun99 <diegun_99@hotmail.com>
2022-06-10 18:15:08 -0500

    navbar agregado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MainComponent } from './main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:MainComponent, children: [
+    {path : 'home', component: HomeComponent},
+    {path : '**' , redirectTo : 'home'}
+  ] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
